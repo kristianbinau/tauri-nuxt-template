4de1eb17223bffe0427ae4ae02d40e37b1fe69ae
Kristian Binau <kristian@binau.me>
2024-10-02 20:15:11 +0200

    Upgrade Tauri to 2.0 Stable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
   // Enable SSG
   ssr: false,
 
+  // Enables the development server to be discoverable by other devices when running on iOS physical devices
+  devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+
   vite: {
     // Better support for Tauri CLI output
     clearScreen: false,
@@ -16,18 +19,7 @@ export default defineNuxtConfig({
     // https://tauri.app/2/reference/environment-variables/
     envPrefix: ['VITE_', 'TAURI_'],
     server: {
-      // Tauri requires a consistent port
       strictPort: true,
-      // Enables the development server to be discoverable by other devices for mobile development
-      host: '0.0.0.0',
-      hmr: {
-        // Use websocket for mobile hot reloading
-        protocol: 'ws',
-        // Make sure it's available on the network
-        host: '0.0.0.0',
-        // Use a specific port for hmr
-        port: 5183,
-      },
     },
   },
 
